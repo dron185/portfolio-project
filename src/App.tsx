137f44d04a264887b346fb73e15164fcc2310fd9
Andrei Huryn <gurin.nord@gmail.com>
2024-02-02 17:16:28 +0300

    remove comment from app

--- src/App.tsx
+++ src/App.tsx
@@ -15,20 +15,3 @@ function App() {
 }
 
 export default App;
-
-/*
-const Title = styled.h1`
-    font-size: 1.5em;
-    text-align: center;
-    color: #e91e63;
-`;
-
-const StyledAnxiety = styled.div`
-  background-color: black;
-  padding: 20px;
-  border: 1px solid red;
-  border-radius: 15px;
-  display: flex;
-  gap: 20px;
-  align-items: flex-start;
-`*/
