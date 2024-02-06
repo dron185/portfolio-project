c247761bd490489461ae90ca1619d69d4e80aa3b
Andrei Huryn <gurin.nord@gmail.com>
2024-02-06 21:10:41 +0300

    add styles to main section

--- src/layout/sections/main/Main.tsx
+++ src/layout/sections/main/Main.tsx
@@ -2,18 +2,25 @@ import React from 'react';
 import photo from '../../../assets/images/photo-1.jpg'
 import styled from "styled-components";
 import {FlexWrapper} from "../../../components/FlexWrapper";
+import {Container} from "../../../components/Container";
+import {theme} from "../../../styles/Theme";
 
 export const Main = () => {
     return (
         <StyledMain>
-            <FlexWrapper justify={"space-around"} align={"center"}>
-                <div>
-                    <span>Hi There</span>
-                    <Name>I am Andrei Huryn</Name>
-                    <MainTitle>A Frontend Developer.</MainTitle>
-                </div>
-                <Photo src={photo} alt=""/>
-            </FlexWrapper>
+            <Container>
+                <FlexWrapper justify={"space-between"} align={"center"}>
+                    <div>
+                        <SmallText>Hi There</SmallText>
+                        <Name>I am <span>Andrei Huryn</span></Name>
+                        <MainTitle>A Frontend Developer.</MainTitle>
+                    </div>
+
+                    <PhotoWrapper>
+                        <Photo src={photo} alt=""/>
+                    </PhotoWrapper>
+                </FlexWrapper>
+            </Container>
         </StyledMain>
     );
 };
@@ -21,6 +28,25 @@ export const Main = () => {
 const StyledMain = styled.section`
     min-height: 100vh;
     background-color: #f6f3b6;
+    display: flex;
+`
+
+const PhotoWrapper = styled.div`
+    position: relative;
+    z-index: 0;
+    
+    &::before {
+        content: '';
+        width: 387px;
+        height: 387px;
+        border-radius: 50%;
+        border: 5px solid ${theme.colors.accent1};
+        
+        position: absolute;
+        top: -10px;
+        left: 10px;
+        z-index: -1;
+    }
 `
 
 const Photo = styled.img`
@@ -31,9 +57,38 @@ const Photo = styled.img`
 `
 
 const MainTitle = styled.h1`
-    
+    font-size: 27px;
+    font-weight: 400;
+    line-height: 1.5;
+`
+
+const SmallText = styled.span`
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 1.5;
 `
 
 const Name = styled.h2`
+    margin: 10px 0;
+    font-family: 'Josefin Sans', sans-serif;
+    font-size: 50px;
+    font-weight: 700;
+    letter-spacing: 0.05em;
     
+    span {
+        position: relative;
+        z-index: 0;
+        
+        &::before {
+            content: '';
+            display: inline-block;
+            width: 100%;
+            height: 20px;
+            background-color: ${theme.colors.accent1};
+            
+            position: absolute;
+            bottom: 0;
+            z-index: -1;
+        }
+    }
 `
--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -2,7 +2,9 @@ export const theme = {
     colors: {
         primaryBg: "rgb(34, 42, 54)",
         secondaryBg: "rgba(28,40,56,0.93)",
-        accent: "#7572D5",
+        // accent: "#7572D5",
+        accent: "#E3CB9D",
+        accent1: "#245741",
         font: "#ffffff"
     }
 }
