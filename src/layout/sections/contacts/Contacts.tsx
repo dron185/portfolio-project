723b6daf69ba0149e1ddca1feb65624383fcc21a
Andrei Huryn <gurin.nord@gmail.com>
2024-02-08 11:00:38 +0300

    add styles to contacts section

--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -2,17 +2,21 @@ import React from 'react';
 import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {Button} from "../../../components/Button";
+import {Container} from "../../../components/Container";
+import {theme} from "../../../styles/Theme";
 
 export const Contact = () => {
     return (
         <StyledContacts>
-            <SectionTitle>Contact</SectionTitle>
-            <StyledForm>
-                <Field placeholder={"Name"}/>
-                <Field placeholder={"Subject"}/>
-                <Field placeholder={"Message"} as={"textarea"}/>
-                <Button type={"submit"}>Send message</Button>
-            </StyledForm>
+            <Container>
+                <SectionTitle>Contact</SectionTitle>
+                <StyledForm>
+                    <Field placeholder={"Name"}/>
+                    <Field placeholder={"Subject"}/>
+                    <Field placeholder={"Message"} as={"textarea"}/>
+                    <Button type={"submit"}>Send message</Button>
+                </StyledForm>
+            </Container>
         </StyledContacts>
     );
 };
@@ -26,8 +30,33 @@ const StyledForm = styled.form`
     margin: 0 auto;
     display: flex;
     flex-direction: column;
-    gap: 10px;
+    align-items: center;
+    gap: 16px;
+    
+    textarea {
+        height: 155px;
+        resize: none;
+    }
 `
 const Field = styled.input`
+    width: 100%;
+    border-radius: 6px;
+    padding: 7px 15px;
+    border: 1px solid ${theme.colors.borderColor};
     
+    font-family: 'Poppins', sans-serif;
+    font-size: 12px;
+    font-weight: 400;
+    line-height: 1.5;
+    letter-spacing: 0.05em;
+    color: ${theme.colors.font};
+    background-color: ${theme.colors.primaryBg};
+
+    &::placeholder {
+        color: ${theme.colors.borderColor};
+    }
+    
+    &:focus-visible {
+        outline: 1px solid ${theme.colors.borderColor};
+    }
 `
--- src/styles/Theme.ts
+++ src/styles/Theme.ts
@@ -2,10 +2,9 @@ export const theme = {
     colors: {
         primaryBg: "#222A36",
         secondaryBg: "#2e3949",
-        // accent: "#7572D5",
         accent: "#E3CB9D",
-        // accent1: "#245741",
         accent1: "#01796F",
-        font: "#ffffff"
+        font: "#ffffff",
+        borderColor: "#81796a"
     }
 }
