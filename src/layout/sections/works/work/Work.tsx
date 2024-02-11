015f16d5e70c92974cab64ab111be21fd73dc13b
Andrei Huryn <gurin.nord@gmail.com>
2024-02-11 13:52:09 +0300

    feat: add responsiveness and media to works section

--- src/layout/sections/works/work/Work.tsx
+++ src/layout/sections/works/work/Work.tsx
@@ -32,9 +32,10 @@ export const Work = (props: WorkPropsType) => {
 };
 
 const StyledWork = styled.div`
+    width: 330px;
+    flex-grow: 1;
     background-color: ${theme.colors.secondaryBg};
-    max-width: 540px;
-    width: 100%;
+    
     display: flex;
     flex-direction: column;
     
@@ -45,39 +46,58 @@ const StyledWork = styled.div`
             margin-left: 20px;
         }
     }
+    
+    @media ${theme.media.desktop} {
+        max-width: 540px;
+    }
 `
 
 const ImageWrapper = styled.div`
     position: relative;
     
-    &:hover {
-        &::before {
-            content: '';
-            position: absolute;
-            left: 0;
-            right: 0;
-            top: 0;
-            bottom: 0;
-            backdrop-filter: blur(5px);
-            background: rgba(0, 0, 0, 0.3);
-        }
-        
-        ${Button} {
-            opacity: 1;
-        }
-    }
-
     ${Button} {
         opacity: 0;
         position: absolute;
         left: 50%;
         top: 50%;
         transform: translate(-50%, -50%);
-        
+
         &::before {
             width: 100%;
             height: 100%;
             border-radius: 6px;
+        }
+    }
+
+    &::before {
+        content: '';
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        backdrop-filter: blur(5px);
+        background: rgba(0, 0, 0, 0.3);
+        opacity: 0;
+    }
+    
+    &:hover {
+        &::before {
+            opacity: 1;
+        }
+        
+        ${Button} {
+            opacity: 1;
+        }
+    }
+    
+    @media ${theme.media.tablet} {
+        &::before {
+            opacity: 1;
+        }
+
+        ${Button} {
+            opacity: 1;
         }
     }
 `
