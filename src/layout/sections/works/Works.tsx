80631ee97569114111b6e9a7f7807a889f830c3b
Andrei Huryn <gurin.nord@gmail.com>
2024-02-12 18:13:33 +0300

    feat: refactoring works section

--- src/layout/sections/works/Works.tsx
+++ src/layout/sections/works/Works.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import styled from "styled-components";
 import {SectionTitle} from "../../../components/SectionTitle";
 import {FlexWrapper} from "../../../components/FlexWrapper";
 import {Work} from "./work/Work";
@@ -7,30 +6,37 @@ import socialImg from "../../../assets/images/proj-1.png"
 import timerImg from "../../../assets/images/proj-2.png"
 import {Container} from "../../../components/Container";
 import {TabMenu} from "./tabMenu/TabMenu";
+import { S } from './Works_Styles';
 
 const worksItems = ["All", "landing page", "React", "spa"]
 
-export const Works = () => {
+const workData = [
+    {
+        title: "Social Network",
+        src: socialImg,
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+    },
+    {
+        title: "Timer",
+        src: timerImg,
+        text: "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim"
+    }
+]
+
+export const Works: React.FC = () => {
     return (
-        <StyledWorks>
+        <S.Works>
             <Container>
                 <SectionTitle>My Works</SectionTitle>
                 <TabMenu menuItems={worksItems}/>
                 <FlexWrapper justify={"space-between"} wrap={"wrap"}>
-                    <Work title={"Social Network"}
-                          text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit."}
-                          src={socialImg }/>
-                    <Work title={"Timer"}
-                          text={"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing elit  ut labore et dolore magna aliqua Ut enim"}
-                          src={timerImg}/>
+
+                    {workData.map((w, index) => {
+                        return <Work key={index} title={w.title} src={w.src} text={w.text}/>
+                    })}
+
                 </FlexWrapper>
             </Container>
-        </StyledWorks>
+        </S.Works>
     );
 };
-
-const StyledWorks = styled.section`
-    ${FlexWrapper} {
-        gap: 30px;
-    }
-`
--- src/layout/sections/works/work/Work.tsx
+++ src/layout/sections/works/work/Work.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import styled from "styled-components";
 import {Link} from "../../../../components/Link";
-import {theme} from "../../../../styles/Theme";
 import {Button} from "../../../../components/Button";
+import { S } from '../Works_Styles';
 
 type WorkPropsType = {
     title: string
@@ -10,120 +9,22 @@ type WorkPropsType = {
     src: string
 }
 
-export const Work = (props: WorkPropsType) => {
+export const Work: React.FC<WorkPropsType> = (props: WorkPropsType) => {
     return (
-        <StyledWork>
-            <ImageWrapper>
-                <Image src={props.src} alt=""/>
+        <S.Work>
+            <S.ImageWrapper>
+                <S.Image src={props.src} alt=""/>
                 <Button>view project</Button>
-            </ImageWrapper>
-            <Description>
-                <Title>{props.title}</Title>
-                <Text>{props.text}</Text>
+            </S.ImageWrapper>
 
-                <Links>
+            <S.Description>
+                <S.Title>{props.title}</S.Title>
+                <S.Text>{props.text}</S.Text>
+                <S.Links>
                     <Link href={"#"}>demo</Link>
                     <Link href={"#"}>Code</Link>
-                </Links>
-
-            </Description>
-        </StyledWork>
+                </S.Links>
+            </S.Description>
+        </S.Work>
     );
 };
-
-const StyledWork = styled.div`
-    width: 330px;
-    flex-grow: 1;
-    background-color: ${theme.colors.secondaryBg};
-    
-    display: flex;
-    flex-direction: column;
-    
-    ${Link} {
-        padding: 10px 0;
-        
-        & + ${Link} {
-            margin-left: 20px;
-        }
-    }
-    
-    @media ${theme.media.desktop} {
-        max-width: 540px;
-    }
-`
-
-const ImageWrapper = styled.div`
-    position: relative;
-    
-    ${Button} {
-        opacity: 0;
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%, -50%);
-
-        &::before {
-            width: 100%;
-            height: 100%;
-            border-radius: 6px;
-        }
-    }
-
-    &::before {
-        content: '';
-        position: absolute;
-        left: 0;
-        right: 0;
-        top: 0;
-        bottom: 0;
-        backdrop-filter: blur(5px);
-        background: rgba(0, 0, 0, 0.3);
-        opacity: 0;
-    }
-    
-    &:hover {
-        &::before {
-            opacity: 1;
-        }
-        
-        ${Button} {
-            opacity: 1;
-        }
-    }
-    
-    @media ${theme.media.tablet} {
-        &::before {
-            opacity: 1;
-        }
-
-        ${Button} {
-            opacity: 1;
-        }
-    }
-`
-
-const Image = styled.img`
-    width: 100%;
-    height: 260px;
-    object-fit: cover;
-`
-
-const Description = styled.div`
-    padding: 25px 20px;
-    display: flex;
-    flex-direction: column;
-    flex: 1 1 auto;
-`
-
-const Title = styled.h3`
-    margin-bottom: 14px;
-`
-
-const Text = styled.p`
-    margin-bottom: 15px;
-    flex: 1 1 auto;
-`
-
-const Links = styled.div`
-    
-`
