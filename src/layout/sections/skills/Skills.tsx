4187cbb8aea33e82ef8c23d6754501edcd253d8b
Andrei Huryn <gurin.nord@gmail.com>
2024-02-16 11:57:51 +0300

    feat: add scrolling animation react-awesome-reveal to skills section

--- src/layout/sections/skills/Skills.tsx
+++ src/layout/sections/skills/Skills.tsx
@@ -4,6 +4,7 @@ import {SectionTitle} from "../../../components/SectionTitle";
 import { Skill } from './skill/Skill';
 import {Container} from "../../../components/Container";
 import { S } from './Skills_Styles';
+import { Fade } from "react-awesome-reveal";
 
 const skillData = [
     {
@@ -49,10 +50,11 @@ export const Skills = () => {
             <Container>
                 <SectionTitle>My Skills</SectionTitle>
                 <FlexWrapper wrap={"wrap"} justify={"space-between"}>
-
-                    {skillData.map((s, index) => {
-                        return <Skill key={index} iconId={s.iconId} title={s.title} description={s.description}/>
-                    })}
+                    <Fade cascade={true} damping={0.2}>
+                        {skillData.map((s, index) => {
+                            return <Skill key={index} iconId={s.iconId} title={s.title} description={s.description}/>
+                        })}
+                    </Fade>
 
                 </FlexWrapper>
             </Container>
